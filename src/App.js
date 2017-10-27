693029c67eb4e882f54cfb5dcf473c6ef9880591
macro6461 <mattcroak718@gmail.com>
2017-10-27 15:16:12 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import Counter from './components/Counter'
 
 class App extends Component {
+
   render() {
+    console.log(this.props.store)
     return (
       <div className="App" >
-        Counter Component here.
+        <Counter store={this.props.store}/>
       </div>
     );
   }
